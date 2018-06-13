73be10263079c123c47c1f51a3861051f0a2cce7
Coley Shi <shikenian@126.com>
2018-06-13 16:07:45 +0800

    This is used to do the simple test of spark

---
+++ SimpleApp.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+
+logFile = "/bigdata/spark-2.3.0-bin-hadoop2.6/README.md"  # Should be some file on your system
+spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+logData = spark.read.text(logFile).cache()
+
+numAs = logData.filter(logData.value.contains('a')).count()
+numBs = logData.filter(logData.value.contains('b')).count()
+
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+
+spark.stop()
